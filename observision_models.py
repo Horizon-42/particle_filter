Increase noise covariance in NormalTransition and fix noise generation in noisy_propagate method
- Updated the diagonal covariance matrix Q from 500 to 1000 to increase noise levels.
- Modified the noisy_propagate method to correctly generate noise for multiple particles, ensuring that noise is not repeated.
- Added a test case in the main block to compare the mean squared difference between the noisy and non-noisy transitions.

observision_models.py
@@ -7,7 +7,7 @@ class NormalObservation:
     C = np.array([[1, 0, 0, 0],
                   [0, 1, 0, 0]], dtype=float)
 
-    R = random_diagonal_cov(2, 10000)
+    R = random_diagonal_cov(2, 100)
 
     noise_distribution = multivariate_normal([0]*2, R)
 
@@ -70,6 +70,8 @@ class NormalObservation:
 
         log_likelihoods = multivariate_normal_logpdf_vectorized(
             single_observe.flatten(), expected_observations, cls.R)
+        print(
+            f"loglikelihood max:{np.max(log_likelihoods)}, min{np.min(log_likelihoods)}, mean:{np.mean(log_likelihoods)}")
 
         max_log_likelihood = np.max(log_likelihoods)
 

partickle_filter.py
@@ -12,8 +12,8 @@ class ParticleFilter:
         self.particles = np.random.rand(self.N, 4, 1)
         self.particles[:, 0] *= 300  # x position in [0, 3000]
         self.particles[:, 1] *= 300  # y position in [0, 3000]
-        self.particles[:, 2] *= 200 # velocity in [0, 200]
-        self.particles[:, 3] *= 200 # velocity in [0, 200]
+        self.particles[:, 2] *= 2000  # velocity in [0, 200]
+        self.particles[:, 3] *= 2000  # velocity in [0, 200]
 
         self.weights = np.ones(self.N) / self.N  # uniform weights
         self.snaps = [(self.particles, self.weights)]
@@ -45,6 +45,9 @@ class ParticleFilter:
         return particles[indices]  # Select particles using the found indices
 
     def update(self, particles: np.ndarray, weights: np.ndarray, observation: np.ndarray):
+        print(f"Neff:{1/np.sum(weights**2)}")
+        print(
+            f"weighs max:{np.max(weights)}, min:{np.min(weights)}, mean:{np.mean(weights)}")
         # sample from st-1
         new_particles = self.systematic_resample(particles, weights)
 

transition_models.py
@@ -22,7 +22,15 @@ class NormalTransition:
     action = np.array([0, -10, 0]).reshape((3,1))
 
     # sigma for normal noise
-    Q = random_diagonal_cov(4, 500)
+    Q = random_diagonal_cov(4, 1000)
+    # print(Q)
+    # Q = np.eye(4)
+    # Q_diag_x = 1000.0  # 位置维度可以有较大的噪声，用于探索
+    # Q_diag_y = 1000.0
+    # Q_diag_vx = 100.0  # 速度维度可以有相对较小的噪声
+    # Q_diag_vy = 100.0
+    # Q = np.diag([Q_diag_x, Q_diag_y, Q_diag_vx, Q_diag_vy])
+
 
     @classmethod
     def propagate(cls, state:np.ndarray):
@@ -30,10 +38,19 @@ class NormalTransition:
     
     @classmethod
     def noisy_propagate(cls, state:np.ndarray):
-        return cls.propagate(state) + np.random.multivariate_normal([0]*4, cls.Q).reshape(-1, 1)
+        # wrong approach, reapte one noise
+        # return cls.propagate(state) + np.random.multivariate_normal([0]*4, cls.Q).reshape(-1, 1)
+        N_particles = state.shape[0]
+        D_state = state.shape[1]
+
+        noise = np.random.multivariate_normal(
+            np.zeros(D_state), cls.Q, size=N_particles).reshape(N_particles, D_state, 1)
+        return cls.propagate(state) + noise
 
 
 if __name__ == "__main__":
     states = np.random.rand(10, 4, 1)
     transed = NormalTransition.propagate(states)
     print(transed)
+    noise_transed = NormalTransition.noisy_propagate(states)
+    print(np.mean(noise_transed**2-transed**2))